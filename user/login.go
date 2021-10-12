72b247dfce32c3264ccf1784a4765f97587fbbf8
Natesh Relhan <nateshrelhan@Nateshs-MacBook-Pro.local>
2021-10-12 17:57:33 +0530

    login API added, testing pending

--- user/login.go
+++ user/login.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/nateshr/likeminds-authentication/api_client"
 	"github.com/nateshr/likeminds-authentication/token"
 	"github.com/nateshr/likeminds-authentication/utils"
 	"net/http"
@@ -16,14 +17,6 @@ type LoginRequest struct {
 
 //Login used when user is signing up and generate LTM and RTM tokens
 func Login(c *gin.Context) {
-	var lr LoginRequest
-	if err := c.ShouldBindJSON(&lr); err != nil {
-		c.JSON(http.StatusUnprocessableEntity, utils.Response{
-			Success:      false,
-			ErrorMessage: "Invalid JSON Request!",
-		})
-		return
-	}
 	vtm, ok := c.MustGet("vtm").(*token.VerifyTokenMeta)
 	if !ok {
 		c.JSON(http.StatusInternalServerError, utils.Response{
@@ -32,34 +25,87 @@ func Login(c *gin.Context) {
 		})
 		return
 	}
-
-	//TODO - call api/user/login and get response by sending mobile no and country code from verifyOTPTokenMeta
-	success := true
-	errorMessage := ""
-	userID := float64(456)
-	if !success {
-		c.JSON(http.StatusInternalServerError, utils.Response{
+	var lr LoginRequest
+	if err := c.ShouldBindJSON(&lr); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, utils.Response{
 			Success:      false,
-			ErrorMessage: errorMessage,
+			ErrorMessage: "Body params missing!",
 		})
 		return
 	}
 
-	//Create login and refresh token meta from the response received in api/user/login
-	ltm, rtm, err := token.CreateLTMAndRTM(vtm.VerifiedMobileNo, vtm.CountryCode, userID)
+	//Params to be sent in the request
+	params, err := utils.RequestParamsToMap(lr)
 	if err != nil {
-		c.JSON(http.StatusUnprocessableEntity, utils.Response{
+		c.JSON(http.StatusInternalServerError, utils.Response{
+			Success:      false,
+			ErrorMessage: "Something went wrong! Please try after sometime",
+		})
+	}
+	//http client and request options
+	client := api_client.NewAPIClient()
+	options := api_client.GetRequestOptions{
+		Url:           client.CoreServiceBaseURL + "/api/user/login",
+		Params:        params,
+		CustomHeaders: nil,
+	}
+	//Unmarshaling of response
+	respBytes, err := client.GetRequest(&options)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, utils.Response{
 			Success:      false,
 			ErrorMessage: err.Error(),
 		})
 		return
 	}
-	token := map[string]string{
-		"access_token":  ltm.AccessToken,
-		"refresh_token": rtm.RefreshToken,
+	var apiCR api_client.APIClientResponse
+	err = api_client.UnmarshalAPIClientResponse(respBytes, &apiCR)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, utils.Response{
+			Success:      false,
+			ErrorMessage: "Something went wrong! Please try after sometime",
+		})
 	}
-	c.JSON(http.StatusOK, utils.Response{
-		Success: true,
-		Data:    token,
-	})
+
+	//Check api/user/login success and response
+	if !apiCR.Success {
+		c.JSON(http.StatusInternalServerError, apiCR)
+		return
+	}
+	emailExists := apiCR.Response["email_exists"].(bool)
+	mobileNo := vtm.VerifiedMobileNo
+	countryCode := vtm.CountryCode
+	if emailExists {
+		//Create verify tokenResponse meta from the response received in VTM
+		vtm, err := token.CreateVTM(mobileNo, countryCode)
+		if err != nil {
+			c.JSON(http.StatusUnprocessableEntity, err.Error())
+			return
+		}
+		dataResponse := apiCR.Response
+		dataResponse["access_token"] = vtm.AccessToken
+		c.JSON(http.StatusOK, utils.Response{
+			Success: true,
+			Data:    dataResponse,
+		})
+	} else {
+		userID := apiCR.Response["user"].(map[string]interface{})["id"].(float64)
+		//Create login and refresh dataResponse meta from the response received in api/verify_otp
+		ltm, rtm, err := token.CreateLTMAndRTM(mobileNo, countryCode, userID)
+		if err != nil {
+			c.JSON(http.StatusUnprocessableEntity, utils.Response{
+				Success:      false,
+				ErrorMessage: err.Error(),
+			})
+			return
+		}
+		dataResponse := apiCR.Response
+		dataResponse["access_token"] = ltm.AccessToken
+		dataResponse["refresh_token"] = rtm.RefreshToken
+		c.JSON(http.StatusOK, utils.Response{
+			Success: true,
+			Data:    dataResponse,
+		})
+	}
+	return
 }
---
+++ utils/utils.go
@@ -0,0 +1,13 @@
+package utils
+
+import "encoding/json"
+
+//RequestParamsToMap Converts a struct to a map while maintaining the json alias as keys
+func RequestParamsToMap(obj interface{}) (newMap map[string]string, err error) {
+	data, err := json.Marshal(obj) // Convert to a json string
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(data, &newMap) // Convert to a map
+	return
+}
