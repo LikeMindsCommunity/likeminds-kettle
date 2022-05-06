c7d17753bb134e790cdbca8ed3be304184d02613
Ketan Yadav <ketan@likeminds.community>
2022-05-06 15:49:30 +0530

    Add SDK login flow

--- user/login_old.go
+++ user/login_old.go
@@ -9,38 +9,43 @@ import (
 	"github.com/nateshr/likeminds-authentication/utils"
 )
 
-type LoginRequest struct {
-	APIKey       string `json:"api_key"`
-	LoginType    string `json:"type" binding:"required"`
-	UserName     string `json:"user_name" binding:"required"`
-	UserUniqueID string `json:"user_unique_id"`
+type LoginRequestOld struct {
+	UserAcquisitionURL string `json:"user_acquisition_url"`
+	LoginJSON          string `json:"login_json"`
+	GoogleIDToken      string `json:"google_id_token"`
+	LoginType          string `json:"type" binding:"required"`
 }
 
 //Login used when user is signing up and generate login and refresh tokens
-func Login(c *gin.Context) {
+func LoginOld(c *gin.Context) {
 	//POST body params
-	var lr LoginRequest
+	var lr LoginRequestOld
 	if err := c.ShouldBindJSON(&lr); err != nil {
 		//If POST body params are missing
 		utils.POSTBodyParamsMissingError(c)
 		return
 	}
 
+	//Params to be sent in the api/user/login
+	params, err := utils.RequestParamsToMap(lr)
+	if err != nil {
+		//If mapping fails
+		utils.GeneralAPIError(c, err.Error())
+	}
 	//Create internal API client
 	client := api_client.NewAPIClient()
-	options := api_client.PostRequestOptions{
+	options := api_client.GetRequestOptions{
 		Url:           client.CoreServiceBaseURL + LoginEndPoint,
-		Body:          lr,
+		Params:        params,
 		CustomHeaders: nil,
 	}
 	//Send request
-	respBytes, err := client.PostRequest(&options)
+	respBytes, err := client.GetRequest(&options)
 	if err != nil {
 		//If API fails or any other error
 		utils.GeneralAPIError(c, err.Error())
 		return
 	}
-
 	//Parse response
 	var apiCR api_client.APIClientResponse
 	err = api_client.UnmarshalAPIClientResponse(respBytes, &apiCR)
@@ -54,12 +59,10 @@ func Login(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, apiCR)
 		return
 	}
-
 	//If flow succeeds
-	userID := apiCR.Response[ResponseUser].(map[string]interface{})[ResponseUserUniqueId].(string)
+	userID := apiCR.Response[ResponseUser].(map[string]interface{})[ResponseUserUniqueId].(float64)
 	//Create login and refresh token
-
-	ltm, rtm, err := token.CreateLTMAndRTM(userID)
+	ltm, rtm, err := token.CreateLTMAndRTM(utils.FormatFloat(userID, 0))
 	if err != nil {
 		//If token creation fails
 		utils.GeneralAPIError(c, err.Error())
