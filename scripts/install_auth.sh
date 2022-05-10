701995b63b40217d798b8991305aa28dd98b5f02
Natesh Relhan <nateshrelhan@Nateshs-MacBook-Pro.local>
2022-05-10 10:11:10 +0530

    workspace path added in build_auth

--- scripts/install_auth.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/share/rvm/bin:/usr/local/go/bin:/home/ubuntu/work/bin
--- scripts/start_auth.sh
+++ scripts/start_auth.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-systemctl restart auth
+sudo systemctl restart auth
