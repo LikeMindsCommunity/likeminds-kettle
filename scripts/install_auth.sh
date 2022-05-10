52769bc89e9ed37da260521c764a6367f573c4f7
Natesh Relhan <nateshrelhan@Nateshs-MacBook-Pro.local>
2022-05-10 09:54:20 +0530

    install auth script added

---
+++ scripts/install_auth.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/share/rvm/bin:/usr/local/go/bin:/home/ubuntu/work/bin
