78b5196c729098622a9f409ce004d12e51e7db3c
Mahir-G2 <mahir.gupta@likeminds.community>
2022-05-31 13:44:37 +0530

    added cors settings

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v7"
 	"github.com/nateshr/likeminds-authentication/api_client"
@@ -27,6 +28,7 @@ var (
 
 func main() {
 	client = cache.InitRedis()
+	router.Use(cors.Default())
 	router.Use(ApiMiddleware(client))
 	router.GET("", web.Home)
 	router.GET("/otp/generate", otp.GenerateOTP)
