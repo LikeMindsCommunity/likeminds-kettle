4526ab1c5a3925bafee0b439b5da65882fd8a399
Ketan Yadav <ketan@likeminds.community>
2022-06-01 18:57:54 +0530

    Add stashed changes

---
+++ sdk/project.go
@@ -0,0 +1,192 @@
+package sdk
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/nateshr/likeminds-authentication/api_client"
+	"github.com/nateshr/likeminds-authentication/token"
+	"github.com/nateshr/likeminds-authentication/user"
+	"github.com/nateshr/likeminds-authentication/utils"
+)
+
+//Platform | SDK platform schema
+type Platform struct {
+	Type        int    `json:"type"`
+	Package     string `json:"package"`
+	Certificate string `json:"certificate"`
+}
+
+//CommunityBasicBranding | community basic branding schema
+type CommunityBasicBranding struct {
+	PrimaryColour string `json:"primary_colour"`
+}
+
+//CommunityAdvancedBranding | community advanced branding schema
+type CommunityAdvancedBranding struct {
+	HeaderColour       string `json:"header_colour"`
+	ButtonsIconsColour string `json:"buttons_icons_colour"`
+	TextLinksColour    string `json:"text_links_colour"`
+}
+
+//CommunityBrandingRequest | create SDK api key platform schema
+type CommunityBrandingRequest struct {
+	Basic    CommunityBasicBranding    `json:"basic"`
+	Advanced CommunityAdvancedBranding `json:"advanced"`
+}
+
+//ProjectRequest | create SDK api key request schema
+type ProjectRequest struct {
+	CommunityName  string                   `json:"name" binding:"required"`
+	Branding       CommunityBrandingRequest `json:"branding"`
+	Headline       string                   `json:"headline"`
+	ImageURL       string                   `json:"image_url"`
+	Platform       []Platform               `json:"platform"`
+	ProjectCreator string                   `json:"project_creator"`
+}
+
+//CreateProject is used to create a new sdk project
+func CreateProject(c *gin.Context) {
+	Project(c, utils.POSTMethod)
+}
+
+//EditProject is used to edit an sdk project
+func EditProject(c *gin.Context) {
+	Project(c, utils.PUTMethod)
+}
+
+//GetProject is used to get an existing sdk project
+func GetProject(c *gin.Context) {
+	Project(c, utils.GETMethod)
+}
+
+//DeleteProject is used to delete an existing sdk project
+func DeleteProject(c *gin.Context) {
+	Project(c, utils.DELETEMethod)
+}
+
+//Project method handles community sdk project for each client
+func Project(c *gin.Context, method int) {
+	//Create internal API client
+	client := api_client.NewAPIClient()
+
+	//Check if request has LTM token or not
+	ltm, ok := c.MustGet(token.ParamLTM).(*token.LoginTokenMeta)
+	if !ok {
+		//If token is not available
+		utils.GeneralAPIError(c, utils.ErrorInvalidLTM)
+		return
+	}
+
+	//Send request
+	var respBytes []byte
+	var err error
+	switch method {
+	case utils.GETMethod:
+		//Call GET api/bot to get bot
+		response := user.GetBotResponse(c, utils.GETMethod)
+		if response == nil {
+			return
+		}
+		//Params to be sent in the api/sdk/project GET request
+		params := map[string]string{
+			ParamCommunityCreator: ltm.UserUniqueID,
+		}
+		options := api_client.GetRequestOptions{
+			Url:           client.CoreServiceBaseURL + ProjectEndpoint,
+			CustomHeaders: utils.CreateHeaders(c, user.GetUserUniqueIDFromResponse(response)),
+			Params:        params,
+		}
+		respBytes, err = client.GetRequest(&options)
+	case utils.POSTMethod:
+		//Call POST api/bot to create bot
+		response := user.GetBotResponse(c, utils.POSTMethod)
+		if response == nil {
+			return
+		}
+		//Params to be sent in the api/sdk/project POST request
+		projectRequest, err := parseProjectRequest(c, ltm.UserUniqueID)
+		if err != nil {
+			//If POST body params are missing
+			utils.GeneralAPIError(c, err.Error())
+			return
+		}
+		options := api_client.PostRequestOptions{
+			Url:           client.CoreServiceBaseURL + ProjectEndpoint,
+			Body:          projectRequest,
+			CustomHeaders: utils.CreateHeaders(c, user.GetUserUniqueIDFromResponse(response)),
+		}
+		respBytes, err = client.PostRequest(&options)
+	case utils.PUTMethod:
+		//Call POST api/bot to create bot
+		response := user.GetBotResponse(c, utils.POSTMethod)
+		if response == nil {
+			return
+		}
+		//Params to be sent in the api/sdk/project PUT request
+		projectRequest, err := parseProjectRequest(c, "")
+		if err != nil {
+			//If POST body params are missing
+			utils.GeneralAPIError(c, err.Error())
+			return
+		}
+		options := api_client.PostRequestOptions{
+			Url:           client.CoreServiceBaseURL + ProjectEndpoint,
+			Body:          projectRequest,
+			CustomHeaders: utils.CreateHeaders(c, user.GetUserUniqueIDFromResponse(response)),
+		}
+		respBytes, err = client.PutRequest(&options)
+	case utils.DELETEMethod:
+		//Call GET api/bot to get bot
+		response := user.GetBotResponse(c, utils.GETMethod)
+		if response == nil {
+			return
+		}
+		//api/sdk/project with delete request
+		options := api_client.PostRequestOptions{
+			Url:           client.CoreServiceBaseURL + ProjectEndpoint,
+			CustomHeaders: utils.CreateHeaders(c, user.GetUserUniqueIDFromResponse(response)),
+		}
+		respBytes, err = client.DeleteRequest(&options)
+	}
+
+	if err != nil {
+		//If API fails or any other error
+		utils.GeneralAPIError(c, err.Error())
+		return
+	}
+
+	//Parse response
+	var apiCR api_client.APIClientResponse
+	err = api_client.UnmarshalAPIClientResponse(respBytes, &apiCR)
+	if err != nil {
+		//Internal unmarshal error
+		utils.GeneralAPIError(c, err.Error())
+		return
+	}
+	if !apiCR.Success {
+		//If api/sdk/project returns success as false
+		c.JSON(http.StatusInternalServerError, apiCR)
+		return
+	}
+	//If flow succeeds
+	c.JSON(http.StatusOK, utils.Response{
+		Success: true,
+		Data:    apiCR.Response,
+	})
+}
+
+func parseProjectRequest(c *gin.Context, projectCreatorID string) (*ProjectRequest, error) {
+	//POST body params
+	var spr ProjectRequest
+	if err := c.ShouldBindBodyWith(&spr, binding.JSON); err != nil {
+		return nil, err
+	}
+
+	if len(projectCreatorID) > 0 {
+		spr.ProjectCreator = projectCreatorID
+	}
+
+	return &spr, nil
+}
---
+++ user/bot.go
@@ -0,0 +1,150 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/nateshr/likeminds-authentication/api_client"
+	"github.com/nateshr/likeminds-authentication/token"
+	"github.com/nateshr/likeminds-authentication/utils"
+)
+
+type BotRequest struct {
+	CommunityName string `json:"community_name"`
+}
+
+//CreateBot is used to create bot of a community
+func CreateBot(c *gin.Context) {
+	Bot(c, utils.POSTMethod)
+	return
+}
+
+//EditBot is used to edit bot of a community
+func EditBot(c *gin.Context) {
+	Bot(c, utils.PUTMethod)
+	return
+}
+
+//GetBot is used to get bot of a community
+func GetBot(c *gin.Context) {
+	Bot(c, utils.GETMethod)
+	return
+}
+
+//Bot used to create/edit/get bot details of a community
+func Bot(c *gin.Context, method int) {
+	response := GetBotResponse(c, method)
+	if response != nil {
+		c.JSON(http.StatusOK, response)
+	}
+	return
+}
+
+//GetBotResponse used to get response when api/user/bot is hit internally
+func GetBotResponse(c *gin.Context, method int) *utils.Response {
+	//Create internal API client
+	client := api_client.NewAPIClient()
+
+	//Send request
+	var respBytes []byte
+	var createToken bool
+	switch method {
+	case utils.GETMethod:
+		options := api_client.GetRequestOptions{
+			Url:           client.CoreServiceBaseURL + BotEndpoint,
+			CustomHeaders: utils.CreateHeaders(c, ""),
+		}
+		var err error
+		respBytes, err = client.GetRequest(&options)
+		if err != nil {
+			//If API fails or any other error
+			utils.GeneralAPIError(c, err.Error())
+			return nil
+		}
+	case utils.POSTMethod:
+		createToken = true
+		br, err := parseBotRequest(c)
+		if err != nil {
+			//If POST body params are missing
+			utils.GeneralAPIError(c, err.Error())
+			return nil
+		}
+		options := api_client.PostRequestOptions{
+			Url:           client.CoreServiceBaseURL + BotEndpoint,
+			Body:          br,
+			CustomHeaders: utils.CreateHeaders(c, ""),
+		}
+		respBytes, err = client.PostRequest(&options)
+		if err != nil {
+			//If API fails or any other error
+			utils.GeneralAPIError(c, err.Error())
+			return nil
+		}
+	case utils.PUTMethod:
+		br, err := parseBotRequest(c)
+		if err != nil {
+			//If POST body params are missing
+			utils.GeneralAPIError(c, err.Error())
+			return nil
+		}
+		options := api_client.PostRequestOptions{
+			Url:           client.CoreServiceBaseURL + BotEndpoint,
+			Body:          br,
+			CustomHeaders: utils.CreateHeaders(c, ""),
+		}
+		respBytes, err = client.PutRequest(&options)
+		if err != nil {
+			//If API fails or any other error
+			utils.GeneralAPIError(c, err.Error())
+			return nil
+		}
+	}
+
+	//Parse response
+	var apiCR api_client.APIClientResponse
+	err := api_client.UnmarshalAPIClientResponse(respBytes, &apiCR)
+	if err != nil {
+		//Internal unmarshal error
+		utils.GeneralAPIError(c, err.Error())
+		return nil
+	}
+	if !apiCR.Success {
+		//If api/user/login returns success as false
+		c.JSON(http.StatusInternalServerError, apiCR)
+		return nil
+	}
+	//If flow succeeds
+	dataResponse := apiCR.Response
+	if createToken {
+		userID := apiCR.Response[ResponseUser].(map[string]interface{})[ResponseUserUniqueId].(string)
+		//Create login and refresh token
+		ltm, rtm, err := token.CreateLTMAndRTM(userID)
+		if err != nil {
+			//If token creation fails
+			utils.GeneralAPIError(c, err.Error())
+			return nil
+		}
+		//Send response with login, refresh token and api/user/login response
+		dataResponse[token.ParamAccessToken] = ltm.AccessToken
+		dataResponse[token.ParamRefreshToken] = rtm.RefreshToken
+	}
+
+	return &utils.Response{
+		Success: true,
+		Data:    dataResponse,
+	}
+}
+
+func parseBotRequest(c *gin.Context) (*BotRequest, error) {
+	//POST body params
+	var br BotRequest
+	if err := c.ShouldBindBodyWith(&br, binding.JSON); err != nil {
+		return nil, err
+	}
+	return &br, nil
+}
+
+func GetUserUniqueIDFromResponse(response *utils.Response) string {
+	return response.Data.(map[string]interface{})[ResponseUser].(map[string]interface{})[ResponseUserUniqueId].(string)
+}
