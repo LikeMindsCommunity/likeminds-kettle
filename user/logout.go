267e638db3d3a06f2eba92d5f21fddbe3c57b844
Mahir-G2 <mahir.gupta@likeminds.community>
2022-06-02 14:06:27 +0530

    updated flow of logout api

--- user/logout.go
+++ user/logout.go
@@ -40,34 +40,38 @@ func Logout(c *gin.Context) {
 		return
 	}
 
-	//Create internal API client
-	apiClient := api_client.NewAPIClient()
-	//Send request
-	respBytes, err := apiClient.PostRequest(&api_client.PostRequestOptions{
-		Url:           apiClient.CoreServiceBaseURL + LogoutEndPoint,
-		CustomHeaders: utils.CreateHeaders(c, ltm.UserUniqueID),
-	})
-	if err != nil {
-		//If API fails or any other error
-		utils.GeneralAPIError(c, err.Error())
-		return
-	}
-	//Parse response
-	var apiCR api_client.APIClientResponse
-	err = api_client.UnmarshalAPIClientResponse(respBytes, &apiCR)
-	if err != nil {
-		//Internal unmarshal error
-		utils.GeneralAPIError(c, err.Error())
-	}
+	headers := utils.CreateHeaders(c, ltm.UserUniqueID)
 
-	if !apiCR.Success {
-		//If api/user/logout returns success as false
-		c.JSON(http.StatusInternalServerError, apiCR)
-		return
+	if len(headers[utils.HeadersDeviceId].(string)) > 0 {
+		//Create internal API client
+		apiClient := api_client.NewAPIClient()
+		//Send request
+		respBytes, err := apiClient.PostRequest(&api_client.PostRequestOptions{
+			Url:           apiClient.CoreServiceBaseURL + LogoutEndPoint,
+			CustomHeaders: headers,
+		})
+		if err != nil {
+			//If API fails or any other error
+			utils.GeneralAPIError(c, err.Error())
+			return
+		}
+		//Parse response
+		var apiCR api_client.APIClientResponse
+		err = api_client.UnmarshalAPIClientResponse(respBytes, &apiCR)
+		if err != nil {
+			//Internal unmarshal error
+			utils.GeneralAPIError(c, err.Error())
+		}
+
+		if !apiCR.Success {
+			//If api/user/logout returns success as false
+			c.JSON(http.StatusInternalServerError, apiCR)
+			return
+		}
 	}
 	//If flow succeeds
 	//Blacklist token
-	err = cache.BlacklistToken(client, ltm, rtm)
+	err := cache.BlacklistToken(client, ltm, rtm)
 	if err != nil {
 		//If token blacklist returns error
 		utils.GeneralAPIError(c, err.Error())
