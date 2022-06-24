57dedfc25ae59643ae48c86c24f14b36d19d72c8
Natesh Relhan <nateshrelhan@Nateshs-MacBook-Pro.local>
2022-06-24 13:49:32 +0530

    scripts updated

--- scripts/build_auth.sh
+++ scripts/build_auth.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
+source const.sh
 source /home/ubuntu/.profile
-echo $PATH
 cd /home/ubuntu/LikeMinds-Authentication/
-BETA_ENVIRONMENT=false
 if [ "$BETA_ENVIRONMENT" = true ]
 then
-  export $(curl https://beta-likeminds-media.s3.ap-south-1.amazonaws.com/environment/beta-environment)
   git pull origin development
 else
-  export $(curl https://prod-likeminds-media.s3.ap-south-1.amazonaws.com/environment/prod-environment)
   git pull origin master
 fi
 go build .
--- scripts/start_auth.sh
+++ scripts/start_auth.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+source const.sh
+if [ "$BETA_ENVIRONMENT" = true ]
+then
+  export $(curl https://beta-likeminds-media.s3.ap-south-1.amazonaws.com/environment/beta-environment)
+else
+  export $(curl https://prod-likeminds-media.s3.ap-south-1.amazonaws.com/environment/prod-environment)
+  echo $BETA_ENVIRONMENT
+  echo $GIN_MODE
+fi
 sudo systemctl restart auth
