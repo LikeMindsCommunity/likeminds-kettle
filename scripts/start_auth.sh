154480d88e4b01c5955ac5c01e3be45fdd306c71
Natesh Relhan <nateshrelhan@Nateshs-MacBook-Pro.local>
2022-06-24 13:50:48 +0530

    echo statemnets removed

--- scripts/start_auth.sh
+++ scripts/start_auth.sh
@@ -5,7 +5,5 @@ then
   export $(curl https://beta-likeminds-media.s3.ap-south-1.amazonaws.com/environment/beta-environment)
 else
   export $(curl https://prod-likeminds-media.s3.ap-south-1.amazonaws.com/environment/prod-environment)
-  echo $BETA_ENVIRONMENT
-  echo $GIN_MODE
 fi
 sudo systemctl restart auth
