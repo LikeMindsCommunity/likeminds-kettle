145b667338d3bb47004772e7a8e67432993eaae4
Mahir-G2 <76210826+Mahir-G2@users.noreply.github.com>
2022-09-07 23:16:40 +0530

    [HOTFIX] [LM-7469] Added Version (#90)

--- main.go
+++ main.go
@@ -28,6 +28,8 @@ var (
 )
 
 func main() {
+	var APP_VERSION string = "1.0.1"
+
 	initGin()
 	client = cache.InitRedis()
 	router.Use(cors.New(enableCors()))
@@ -119,6 +121,7 @@ func main() {
 	router.GET("/conversation/notification/unread", LTMValidationMiddleware(client, true), APIKeyValidationMiddleware(), conversation.UnreadConversationNotification)
 	router.GET("/conversation/sync", LTMValidationMiddleware(client, true), APIKeyValidationMiddleware(), conversation.SyncConversation)
 
+	log.Printf("application version: %s", APP_VERSION)
 	log.Fatal(router.Run(":8080"))
 }
 
