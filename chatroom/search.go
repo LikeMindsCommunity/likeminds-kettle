6aa054a9a1622ecfff1770e58afb4138054c9f91
Mahir-G2 <mahir.gupta@likeminds.community>
2022-09-26 16:47:15 +0530

    search feature implemented

---
+++ chatroom/search.go
@@ -0,0 +1,29 @@
+package chatroom
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nateshr/likeminds-authentication/user"
+	"github.com/nateshr/likeminds-authentication/utils"
+)
+
+//ChatroomSearch is used to perform search on the chatrooms
+func ChatroomSearch(c *gin.Context) {
+
+	//Authorize User
+	userId := user.GetRequestingUserId(c)
+	if userId == "" {
+		return
+	}
+
+	//Params to be sent in the chatroom search api internally
+	params := map[string]string{
+		ParamSearch:       c.Query(ParamSearch),
+		ParamFollowStatus: c.Query(ParamFollowStatus),
+		ParamPage:         c.Query(ParamPage),
+		ParamPageSize:     c.Query(ParamPageSize),
+		ParamSearchType:   c.Query(ParamSearchType),
+	}
+
+	//Send Request
+	utils.SendRequest(c, utils.CoreService, ChatroomSearchEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
+}
