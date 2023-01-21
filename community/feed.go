5c20d04db688220067d3e0f9906aaa9b2604a155
Mahir-G2 <mahir.gupta@likeminds.community>
2023-01-21 14:26:55 +0530

    change in structure

--- community/feed.go
+++ community/feed.go
@@ -42,135 +42,23 @@ func CommunityFeed(c *gin.Context, method int) {
 
 		if params[ParamType] == strconv.Itoa(PostFeedType) {
 			if params[ParamOrderType] == strconv.Itoa(OrderTypeNewest) || params[ParamOrderType] == strconv.Itoa(OrderTypeMostParticipants) {
-				//Generate params for CommunityPostFeed Request
-				communityFeedParams := map[string]string{
-					ParamPinned:    params[ParamPinned],
-					ParamOrderType: params[ParamOrderType],
-					ParamPage:      params[ParamPage],
-					ParamPageSize:  params[ParamPageSize],
-				}
-
-				//Send Request to get post community feed on Caravan Service
-				respBytes, statusCode := utils.GetRequestResponse(c, utils.CoreService, CommunityFetchPostFeedEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), communityFeedParams, nil)
-
-				//Validate response
-				apiCR := utils.ValidateClientResponse(c, respBytes, statusCode)
-				if apiCR == nil {
+				//Fetch post dependent data
+				dataResponse := fetchPostDependentFeed(c, userId, params)
+				if dataResponse == nil {
 					return
-				}
-
-				//If flow succeeds
-				dataResponse := apiCR.Response
-				chatroom_ids := []int{}
-
-				chatrooms, ok := dataResponse["chatrooms"]
-				if ok {
-					for _, chatroom := range chatrooms.([]map[string]interface{}) {
-						chatroom_id, ok := chatroom["id"]
-						if ok {
-							convertedChatroomId, _ := strconv.Atoi(chatroom_id.(string))
-							chatroom_ids = append(chatroom_ids, convertedChatroomId)
-						}
-					}
-				}
-
-				temp_params, _ := json.Marshal(chatroom_ids)
-
-				//Generate params for ExploreFeed Request
-				exploreFeedParams := map[string]string{
-					ParamOrderType:   params[ParamOrderType],
-					ParamChatroomIds: fmt.Sprintf("%v", string(temp_params)),
-				}
-
-				//Send Request to get post community feed on Swarm Service
-				respBytes, statusCode = utils.GetRequestResponse(c, utils.SwarmService, feed.FeedExploreEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), exploreFeedParams, nil)
-
-				//Validate response
-				apiCR = utils.ValidateClientResponse(c, respBytes, statusCode)
-				if apiCR == nil {
-					return
-				}
-
-				//If flow succeeds
-				swarmDataResponse := apiCR.Response
-				if postCounts, ok := swarmDataResponse["post_counts"]; ok {
-					dataResponse["post_counts"] = postCounts
 				}
 
 				//Send Response
 				utils.GenerateResponse(c, dataResponse)
 			} else if params[ParamOrderType] == strconv.Itoa(OrderTypeRecentlyActive) || params[ParamOrderType] == strconv.Itoa(OrderTypeMostMessages) {
-				//Send Request to get excluded chatrooms list on Caravan Service
-				respBytes, statusCode := utils.GetRequestResponse(c, utils.CoreService, CommunityExcludedChatroomsEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), nil, nil)
-
-				//Validate response
-				apiCR := utils.ValidateClientResponse(c, respBytes, statusCode)
-				if apiCR == nil {
+				//Fetch post independent data
+				dataResponse := fetchPostIndependentFeed(c, userId, params)
+				if dataResponse == nil {
 					return
-				}
-
-				//If flow succeeds
-				dataResponse := apiCR.Response
-				excludedChatroomIds := []int{}
-
-				chatroomIds, ok := dataResponse["chatroom_ids"]
-				if ok {
-					excludedChatroomIds = chatroomIds.([]int)
-				}
-
-				temp_params, _ := json.Marshal(excludedChatroomIds)
-
-				//Generate params for ExploreFeed Request
-				exploreFeedParams := map[string]string{
-					ParamOrderType:           params[ParamOrderType],
-					ParamPage:                params[ParamPage],
-					ParamPageSize:            params[ParamPageSize],
-					ParamExcludedChatroomIds: fmt.Sprintf("%v", string(temp_params)),
-				}
-
-				//Send Request to get post community feed on Swarm Service
-				respBytes, statusCode = utils.GetRequestResponse(c, utils.SwarmService, feed.FeedExploreEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), exploreFeedParams, nil)
-
-				//Validate response
-				apiCR = utils.ValidateClientResponse(c, respBytes, statusCode)
-				if apiCR == nil {
-					return
-				}
-
-				//If flow succeeds
-				swarmDataResponse := apiCR.Response
-				selectedChatroomIds := []int{}
-
-				chatroomIds, ok = swarmDataResponse["chatroom_ids"]
-				if ok {
-					selectedChatroomIds = chatroomIds.([]int)
-				}
-
-				temp_params, _ = json.Marshal(selectedChatroomIds)
-
-				//Generate params for CommunityPostFeed Request
-				communityFeedParams := map[string]string{
-					ParamOrderType:   params[ParamOrderType],
-					ParamChatroomIds: fmt.Sprintf("%v", string(temp_params)),
-				}
-
-				//Send Request to get post community feed on Caravan Service
-				respBytes, statusCode = utils.GetRequestResponse(c, utils.CoreService, CommunityFetchPostFeedEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), communityFeedParams, nil)
-
-				//Validate response
-				apiCR = utils.ValidateClientResponse(c, respBytes, statusCode)
-				if apiCR == nil {
-					return
-				}
-
-				//If flow succeeds
-				caravanDataResponse := apiCR.Response
-				if postCounts, ok := swarmDataResponse["post_counts"]; ok {
-					caravanDataResponse["post_counts"] = postCounts
 				}
 
 				//Send Response
-				utils.GenerateResponse(c, caravanDataResponse)
+				utils.GenerateResponse(c, dataResponse)
 			} else {
 				utils.GeneralBadRequestError(c, "invalid order_type sent")
 				return
@@ -181,5 +69,136 @@ func CommunityFeed(c *gin.Context, method int) {
 			utils.SendRequest(c, utils.CoreService, CommunityFetchFeedEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
 		}
 	}
+}
 
+func fetchPostDependentFeed(c *gin.Context, userId string, params map[string]string) map[string]interface{} {
+	//Generate params for CommunityPostFeed Request
+	communityFeedParams := map[string]string{
+		ParamPinned:    params[ParamPinned],
+		ParamOrderType: params[ParamOrderType],
+		ParamPage:      params[ParamPage],
+		ParamPageSize:  params[ParamPageSize],
+	}
+
+	//Send Request to get post community feed on Caravan Service
+	respBytes, statusCode := utils.GetRequestResponse(c, utils.CoreService, CommunityFetchPostFeedEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), communityFeedParams, nil)
+
+	//Validate response
+	apiCR := utils.ValidateClientResponse(c, respBytes, statusCode)
+	if apiCR == nil {
+		return nil
+	}
+
+	//If flow succeeds
+	dataResponse := apiCR.Response
+	chatroom_ids := []int{}
+
+	chatrooms, ok := dataResponse["chatrooms"]
+	if ok {
+		for _, chatroom := range chatrooms.([]map[string]interface{}) {
+			chatroom_id, ok := chatroom["id"]
+			if ok {
+				convertedChatroomId, _ := strconv.Atoi(chatroom_id.(string))
+				chatroom_ids = append(chatroom_ids, convertedChatroomId)
+			}
+		}
+	}
+
+	temp_params, _ := json.Marshal(chatroom_ids)
+
+	//Generate params for ExploreFeed Request
+	exploreFeedParams := map[string]string{
+		ParamOrderType:   params[ParamOrderType],
+		ParamChatroomIds: fmt.Sprintf("%v", string(temp_params)),
+	}
+
+	//Send Request to get post community feed on Swarm Service
+	respBytes, statusCode = utils.GetRequestResponse(c, utils.SwarmService, feed.FeedExploreEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), exploreFeedParams, nil)
+
+	//Validate response
+	apiCR = utils.ValidateClientResponse(c, respBytes, statusCode)
+	if apiCR == nil {
+		return nil
+	}
+
+	//If flow succeeds
+	swarmDataResponse := apiCR.Response
+	if postCounts, ok := swarmDataResponse["post_counts"]; ok {
+		dataResponse["post_counts"] = postCounts
+	}
+
+	return dataResponse
+}
+
+func fetchPostIndependentFeed(c *gin.Context, userId string, params map[string]string) map[string]interface{} {
+	//Send Request to get excluded chatrooms list on Caravan Service
+	respBytes, statusCode := utils.GetRequestResponse(c, utils.CoreService, CommunityExcludedChatroomsEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), nil, nil)
+
+	//Validate response
+	apiCR := utils.ValidateClientResponse(c, respBytes, statusCode)
+	if apiCR == nil {
+		return nil
+	}
+
+	//If flow succeeds
+	dataResponse := apiCR.Response
+	excludedChatroomIds := []int{}
+
+	chatroomIds, ok := dataResponse["chatroom_ids"]
+	if ok {
+		excludedChatroomIds = chatroomIds.([]int)
+	}
+
+	temp_params, _ := json.Marshal(excludedChatroomIds)
+
+	//Generate params for ExploreFeed Request
+	exploreFeedParams := map[string]string{
+		ParamOrderType:           params[ParamOrderType],
+		ParamPage:                params[ParamPage],
+		ParamPageSize:            params[ParamPageSize],
+		ParamExcludedChatroomIds: fmt.Sprintf("%v", string(temp_params)),
+	}
+
+	//Send Request to get post community feed on Swarm Service
+	respBytes, statusCode = utils.GetRequestResponse(c, utils.SwarmService, feed.FeedExploreEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), exploreFeedParams, nil)
+
+	//Validate response
+	apiCR = utils.ValidateClientResponse(c, respBytes, statusCode)
+	if apiCR == nil {
+		return nil
+	}
+
+	//If flow succeeds
+	swarmDataResponse := apiCR.Response
+	selectedChatroomIds := []int{}
+
+	chatroomIds, ok = swarmDataResponse["chatroom_ids"]
+	if ok {
+		selectedChatroomIds = chatroomIds.([]int)
+	}
+
+	temp_params, _ = json.Marshal(selectedChatroomIds)
+
+	//Generate params for CommunityPostFeed Request
+	communityFeedParams := map[string]string{
+		ParamOrderType:   params[ParamOrderType],
+		ParamChatroomIds: fmt.Sprintf("%v", string(temp_params)),
+	}
+
+	//Send Request to get post community feed on Caravan Service
+	respBytes, statusCode = utils.GetRequestResponse(c, utils.CoreService, CommunityFetchPostFeedEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), communityFeedParams, nil)
+
+	//Validate response
+	apiCR = utils.ValidateClientResponse(c, respBytes, statusCode)
+	if apiCR == nil {
+		return nil
+	}
+
+	//If flow succeeds
+	caravanDataResponse := apiCR.Response
+	if postCounts, ok := swarmDataResponse["post_counts"]; ok {
+		caravanDataResponse["post_counts"] = postCounts
+	}
+
+	return caravanDataResponse
 }
