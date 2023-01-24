b968730fdad3005f2c736d48927ea3e98b8761f2
ankitgarg <ankit.garg@likeminds.community>
2023-01-24 15:43:24 +0530

    Resolved bug of scientific notation of chatroom ID in /chatroom with DELETE method.

--- chatroom/chatroom.go
+++ chatroom/chatroom.go
@@ -1,6 +1,7 @@
 package chatroom
 
 import (
+	"reflect"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -52,9 +53,9 @@ type EditChatroomRequest struct {
 }
 
 type DeleteChatroomRequest struct {
-	ChatroomID int64  `json:"chatroom_id" binding:"required"`
-	TagID      int32  `json:"tag_id"`
-	Reason     string `json:"reason"`
+	ChatroomID interface{} `json:"chatroom_id" binding:"required"`
+	TagID      int32       `json:"tag_id"`
+	Reason     string      `json:"reason"`
 }
 
 // CreateChatroom is used to create a new chatroom
@@ -226,6 +227,10 @@ func deleteChatroomInternal(c *gin.Context, userId string) {
 		//If POST body params are missing
 		utils.GeneralAPIError(c, err.Error())
 		return
+	}
+
+	if reflect.TypeOf(deleteChatroomRequest.ChatroomID).String() == "float64" {
+		deleteChatroomRequest.ChatroomID = strconv.Itoa(int(deleteChatroomRequest.ChatroomID.(float64)))
 	}
 
 	//Send Request
