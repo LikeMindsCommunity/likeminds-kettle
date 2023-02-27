ffdd9d647010914167c1965f76dc5e989a17ad6a
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-02-27 10:24:58 +0530

    Added Param support for Pagination & search

--- community/tag.go
+++ community/tag.go
@@ -24,12 +24,15 @@ func GetTaggingList(c *gin.Context) {
 	//Params to be sent in the get tag list api internally
 	params := map[string]string{
 		ChatroomIDParam: chatroomId,
+		ParamPage:       c.Query(ParamPage),
+		ParamPageSize:   c.Query(ParamPageSize),
+		SearchName:      c.Query(SearchName),
 	}
 
 	//Params Validation
 	if params[chatroom.ParamChatroomId] == "" {
 		//Send Request
-		utils.SendRequest(c, utils.CoreService, FetchMembersMetaEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), nil, nil)
+		utils.SendRequest(c, utils.CoreService, FetchMembersMetaEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
 	} else {
 		//Send Request
 		utils.SendRequest(c, utils.CoreService, chatroom.GetTaggingListEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
