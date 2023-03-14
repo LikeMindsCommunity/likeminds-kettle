2622704ae826f9f17f9db7c413610a85405af008
ankit-grg <86524580+ankit-grg@users.noreply.github.com>
2023-03-14 17:10:10 +0530

    [LM-8219] Secret chatroom invite misses (#178)

--- chatroom/participants.go
+++ chatroom/participants.go
@@ -78,7 +78,7 @@ func updateParticipantsRequest(pr *ParticipantRequest) *InternalParticipantReque
 
 	ipr.ChatroomID = pr.ChatroomID
 	ipr.SecretChatroomParticipants = pr.ChatroomParticipants
-	ipr.IsChannelInvite = pr.IsChannelInvite
+	ipr.IsChannelInvite = true
 
 	return &ipr
 }
