17bbbac795318138335bebc2b041166cbe91c8bb
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-03-22 13:41:58 +0530

    Internally calling swarm api to delete user data

--- community/member_remove.go
+++ community/member_remove.go
@@ -1,7 +1,10 @@
 package community
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/nateshr/likeminds-authentication/feed"
 	"github.com/nateshr/likeminds-authentication/user"
 	"github.com/nateshr/likeminds-authentication/utils"
 )
@@ -12,7 +15,7 @@ type RemoveMemberRequest struct {
 	Reason    string   `json:"reason"`
 }
 
-//RemoveMember is used to remove a member from community
+// RemoveMember is used to remove a member from community
 func RemoveMember(c *gin.Context) {
 
 	//Authorize User
@@ -34,8 +37,37 @@ func RemoveMember(c *gin.Context) {
 		return
 	}
 
-	//Send Request
-	utils.SendRequest(c, utils.CoreService, RemoveMemberEndPoint, utils.POSTRequestFormUrlEncodedBody, utils.CreateHeaders(c, userId), nil, removeMemberRequest)
+	//Send Request to Main service to remove member
+	respbytes, statusCode := utils.GetRequestResponse(c, utils.CoreService, RemoveMemberEndPoint, utils.POSTRequestFormUrlEncodedBody, utils.CreateHeaders(c, userId), nil, removeMemberRequest)
+
+	// Validate response and if false then return
+	apiCr := utils.ValidateClientResponse(c, respbytes, statusCode)
+	if apiCr == nil {
+		return
+	}
+
+	// If response is successfull
+	user_ids := removeMemberRequest.MemberIds
+
+	// If request is for self removal, then add user id to the list
+	if len(user_ids) == 0 {
+		user_ids = append(user_ids, userId)
+	}
+
+	// create body for user data
+	postBody := map[string]interface{}{
+		"user_ids":   user_ids,
+		"is_user_cm": true,
+	}
+
+	// Send request internally to delete user data
+	_, _, err = utils.GetRequestResponseWithoutContext(utils.SwarmService, feed.DeleteUserDataEndPoint, utils.DELETERequest, utils.CreateHeaders(c, userId), nil, postBody)
+	if err != nil {
+		log.Println("Error while deleting user data from Swarm: ", err)
+	}
+
+	//Generate response
+	utils.GenerateResponse(c, apiCr.Response)
 }
 
 func parseRemoveMemberRequest(c *gin.Context) (*RemoveMemberRequest, error) {
