48d63de912c6a5e71320b6bf497443a8a844b4d0
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-04-06 15:39:41 +0530

    New Parser for Interface to String

--- utils/request_utilties.go
+++ utils/request_utilties.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 )
 
 func ParseArrayToString(array []interface{}) string {
@@ -10,7 +11,27 @@ func ParseArrayToString(array []interface{}) string {
 		This function is used to parse array to string using json marshal
 	*/
 
-	temp_params, _ := json.Marshal(array)
+	temp_params, err := json.Marshal(array)
+	if err != nil {
+		log.Println("Error in parsing array to string: ", err.Error())
+		return ""
+	}
+
+	str := fmt.Sprintf("%v", string(temp_params))
+
+	return str
+}
+
+func ParseInterfaceToString(inter interface{}) string {
+	/*
+		This function is used to parse interface to string using json marshal
+	*/
+
+	temp_params, err := json.Marshal(inter)
+	if err != nil {
+		log.Println("Error in parsing interface to string: ", err.Error())
+		return ""
+	}
 
 	str := fmt.Sprintf("%v", string(temp_params))
 
