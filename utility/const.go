33b53995ca9795e0503c41e1bcb00a6ea0bc5780
ankitgarg <ankit.garg@likeminds.community>
2023-04-06 18:07:14 +0530

    Added client's user unique id support in community/member/remove API.

--- utility/const.go
+++ utility/const.go
@@ -2,5 +2,7 @@ package utility
 
 const DecodeUrlEndPoint = "/api/decode_url"
 const UploadFilesEndpoint = "/api/v1/upload_files"
+const UserMetaInfoEndpoint = "/api/community/users"
 
 const ParamUrl = "url"
+const ParamMemberIDs = "member_ids"
