8c5bdb6ad20dc1170b87f0211707e91f56c3d07e
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-06-08 16:24:19 +0530

    Added users meta in response of APIs

--- search/general.go
+++ search/general.go
@@ -86,13 +86,46 @@ func GeneralSearch(c *gin.Context) {
 		params[ParamExcludedChatroomIds] = fmt.Sprintf("%v", string(temp_params))
 		params[ParamUserIsCm] = fmt.Sprint(response.IsCm)
 
-		respBytes, _, err = utils.GetRequestResponseWithoutContext(utils.SwarmService, PostSearchEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
+		// respBytes, _, err = utils.GetRequestResponseWithoutContext(utils.SwarmService, PostSearchEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
 
-		var apiCR4 api_client.APIClientResponse
-		err = api_client.UnmarshalAPIClientResponse(respBytes, &apiCR4)
+		// var apiCR4 api_client.APIClientResponse
+		// err = api_client.UnmarshalAPIClientResponse(respBytes, &apiCR4)
+		// if err == nil {
+		// 	dataResponse["posts"] = apiCR4.Response["posts"]
+		// }
+
+		//Send Request
+		respBytes, statusCode, err := utils.GetRequestResponseWithoutContext(utils.SwarmService, PostSearchEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
+
+		//Validate response
+		apiCR := utils.ValidateClientResponseWithoutContext(respBytes, statusCode, err)
+
 		if err == nil {
-			dataResponse["posts"] = apiCR4.Response["posts"]
+			dataResponse["posts"] = apiCR["posts"]
+
+			if value, ok := dataResponse["posts"]; ok {
+				posts := value.([]interface{})
+				user_ids := []string{}
+
+				//Fetch posts user id
+				for _, post_data := range posts {
+					if user_unique_id, ok := post_data.(map[string]interface{})["user_id"]; ok {
+						user_ids = append(user_ids, user_unique_id.(string))
+					}
+				}
+
+				//Fetch user data for given user_unique_ids
+				user_data, err := user.FetchMemberMeta(utils.CreateHeaders(c, userId), user_ids)
+				if err != nil {
+					utils.GeneralBadRequestError(c, utils.ErrorFetchingUserData)
+					return
+				}
+
+				//Update user data in dataResponse
+				dataResponse["users"] = user_data
+			}
 		}
+
 	}
 
 	//Generate Response
--- search/post.go
+++ search/post.go
@@ -65,5 +65,41 @@ func PostSearch(c *gin.Context) {
 	}
 
 	//Send Request
-	utils.SendRequest(c, utils.SwarmService, PostSearchEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
+	// utils.SendRequest(c, utils.SwarmService, PostSearchEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
+
+	//Send Request
+	respBytes, statusCode = utils.GetRequestResponse(c, utils.SwarmService, PostSearchEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
+
+	//Validate response
+	apiCR = utils.ValidateClientResponse(c, respBytes, statusCode)
+	if apiCR == nil {
+		return
+	}
+
+	//If flow succeeds
+	dataResponse = apiCR.Response
+	if value, ok := dataResponse["posts"]; ok {
+		posts := value.([]interface{})
+		user_ids := []string{}
+
+		//Fetch posts user id
+		for _, post_data := range posts {
+			if user_unique_id, ok := post_data.(map[string]interface{})["user_id"]; ok {
+				user_ids = append(user_ids, user_unique_id.(string))
+			}
+		}
+
+		//Fetch user data for given user_unique_ids
+		user_data, err := user.FetchMemberMeta(utils.CreateHeaders(c, userId), user_ids)
+		if err != nil {
+			utils.GeneralBadRequestError(c, utils.ErrorFetchingUserData)
+			return
+		}
+
+		//Update user data in dataResponse
+		dataResponse["users"] = user_data
+	}
+
+	//Send response
+	utils.GenerateResponse(c, dataResponse)
 }
