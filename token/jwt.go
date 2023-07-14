24fa4bfdf630481b8db47bbe78af8c87fb8c05bb
Mahir-G2 <76210826+Mahir-G2@users.noreply.github.com>
2023-07-14 12:55:18 +0530

    Revert "[LM - 9184] LTM Expiry Time Change (#286)" (#287)
    This reverts commit a18ebc7b7bbd9da025f0fac21ac6b3fda1506044.

--- token/jwt.go
+++ token/jwt.go
@@ -116,8 +116,8 @@ func CreateLTMAndRTM(userUniqueID string, api_key string) (*LoginTokenMeta, *Ref
 	LTMTokenExpiryTime := time.Duration(15)
 
 	if isBeta == "true" {
-		// Setting LTM token expiry to 1 minutes for Beta
-		LTMTokenExpiryTime = time.Duration(1)
+		// Setting LTM token expiry to 60 minutes for Beta
+		LTMTokenExpiryTime = time.Duration(60)
 	}
 
 	ltm := &LoginTokenMeta{
