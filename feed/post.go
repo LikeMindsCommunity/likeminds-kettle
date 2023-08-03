898c5f7dbd3e0b0ebcb91bd04ec4984c84b80b2c
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-08-03 16:11:26 +0530

    changes

--- feed/post.go
+++ feed/post.go
@@ -38,13 +38,14 @@ type AttachmentRequest struct {
 }
 
 type CreatePostRequest struct {
-	TempID      *string             `json:"temp_id"`
-	TopicIDs    []string            `json:"topic_ids"`
-	Text        string              `json:"text"`
-	Heading     string              `json:"heading"`
-	Attachments []AttachmentRequest `json:"attachments"`
-	FeedroomID  int                 `json:"feedroom_id"`
-	UUIDs       []string            `json:"uuids"`
+	TempID         *string             `json:"temp_id"`
+	TopicIDs       []string            `json:"topic_ids"`
+	Text           string              `json:"text"`
+	Heading        string              `json:"heading"`
+	Attachments    []AttachmentRequest `json:"attachments"`
+	FeedroomID     int                 `json:"feedroom_id"`
+	UUIDs          []string            `json:"uuids"`
+	OnBehalfOfUUID string              `json:"on_behalf_of_uuid"`
 }
 
 type EditPostRequest struct {
@@ -250,6 +251,23 @@ func createPostInternal(c *gin.Context, userId string) {
 		return
 	}
 
+	if createPostRequest.OnBehalfOfUUID != "" {
+		// Check if user is a admin or not
+		if !response.IsCm {
+			utils.MemberAccessFailError(c)
+			return
+		}
+
+		// Get valid user unique ids by calling internal users meta api
+		OnBehalfOfUUID, err := utility.GetUuidInternally(utils.CreateHeaders(c, userId), createPostRequest.OnBehalfOfUUID)
+		if err != nil {
+			utils.GeneralBadRequestError(c, "Invalid UUUD for on_behalf_of_uuid")
+			return
+		}
+
+		createPostRequest.OnBehalfOfUUID = OnBehalfOfUUID
+	}
+
 	//If not access
 	if !response.Access {
 		utils.MemberAccessFailError(c)
@@ -287,7 +305,7 @@ func createPostInternal(c *gin.Context, userId string) {
 	}
 
 	//Generate Response
-	utils.GenerateResponse(c, dataResponse)
+	utils.GenerateResponse(c)
 }
 
 func editPostInternal(c *gin.Context, userId string) {
