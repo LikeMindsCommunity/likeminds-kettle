f3f028237b5d5a6dffb9ee8379584443fb74dcef
shubh-gupta00 <shubh.gupta@likeminds.community>
2023-08-08 11:24:35 +0530

    reverted formatting changes

--- community/report.go
+++ community/report.go
@@ -248,7 +248,6 @@ func closeReportsInternalOld(c *gin.Context, userId string) {
 
 	//Body to be sent in the close report api internally
 	closeReportRequest, err := parseCloseReportRequest(c)
-
 	if err != nil {
 		//If POST body params are missing
 		utils.GeneralAPIError(c, err.Error())
@@ -264,7 +263,6 @@ func closeReportsInternalV1(c *gin.Context, userId string) {
 
 	//Body to be sent in the close report api internally
 	closeReportV1Request, err := parseCloseReportV1Request(c)
-
 	if err != nil {
 		//If POST body params are missing
 		utils.GeneralAPIError(c, err.Error())
