4480b379d377c16a93c88d84909c69b587944a4d
Natesh Relhan <nateshrelhan@Likemindss-MacBook-Pro.local>
2023-08-24 14:00:11 +0530

    request per second tested

--- utility/monitoring/middleware.go
+++ utility/monitoring/middleware.go
@@ -9,13 +9,21 @@ import (
 func PrometheusMiddleware(metricService MetricService) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		responseTimeMetric := newResponseTimeMetric(c.FullPath(), c.Request.Method)
+		//Start recording response time
 		responseTimeMetric.Started()
+		//Increase concurrency
+		metricService.IncreaseConcurrentRequest()
+		//Serve request
 		c.Next()
+		statusCode := strconv.Itoa(c.Writer.Status())
+		responseTimeMetric.StatusCode = statusCode
+		//Finish recording response time
 		responseTimeMetric.Finished()
-		responseTimeMetric.StatusCode = strconv.Itoa(c.Writer.Status())
 		//Save response time
 		metricService.SaveResponseTime(responseTimeMetric)
 		//Save total no. of request
-		metricService.SaveTotalRequest(newTotalRequestMetric(c.FullPath(), c.Request.Method, strconv.Itoa(c.Writer.Status())))
+		metricService.SaveTotalRequest(newTotalRequestMetric(c.FullPath(), c.Request.Method, statusCode))
+		//Decrease concurrency
+		metricService.DecreaseConcurrentRequest()
 	}
 }
--- utility/monitoring/prometheus.go
+++ utility/monitoring/prometheus.go
@@ -7,12 +7,15 @@ import (
 type MetricService interface {
 	SaveResponseTime(responseTimeMetric *ResponseTimeMetric)
 	SaveTotalRequest(totalRequestMetric *TotalRequestMetric)
+	IncreaseConcurrentRequest()
+	DecreaseConcurrentRequest()
 }
 
 // PrometheusService implements UseCase interface
 type PrometheusService struct {
-	responseTimeHistogram *prometheus.HistogramVec
-	totalRequestCounter   *prometheus.CounterVec
+	responseTimeHistogram  *prometheus.HistogramVec
+	totalRequestCounter    *prometheus.CounterVec
+	concurrentRequestGauge prometheus.Gauge
 }
 
 // SaveResponseTime update response time
@@ -21,37 +24,57 @@ func (s *PrometheusService) SaveResponseTime(responseTimeMetric *ResponseTimeMet
 }
 
 // SaveTotalRequest update total no. of requests
-func (s *PrometheusService) SaveTotalRequest(responseTimeMetric *TotalRequestMetric) {
-	s.totalRequestCounter.WithLabelValues(responseTimeMetric.Handler, responseTimeMetric.Method, responseTimeMetric.StatusCode).Inc()
+func (s *PrometheusService) SaveTotalRequest(totalRequestMetric *TotalRequestMetric) {
+	s.totalRequestCounter.WithLabelValues(totalRequestMetric.Handler, totalRequestMetric.Method, totalRequestMetric.StatusCode).Inc()
+}
+
+// IncreaseConcurrentRequest update concurrent request
+func (s *PrometheusService) IncreaseConcurrentRequest() {
+	s.concurrentRequestGauge.Inc()
+}
+
+// DecreaseConcurrentRequest update concurrent request
+func (s *PrometheusService) DecreaseConcurrentRequest() {
+	s.concurrentRequestGauge.Dec()
 }
 
 // NewPrometheusService create a new prometheus service
 func NewPrometheusService() (*PrometheusService, error) {
 	responseTimeHistogram := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: "http",
-		Name:      "request_duration_seconds",
-		Help:      "The latency of the ResponseTimeMetric requests.",
+		Namespace: "kettle",
+		Name:      "response_time",
+		Help:      "The latency of requests.",
 		Buckets:   prometheus.DefBuckets,
 	}, []string{"handler", "method", "code"})
 
-	var totalRequestCounter = prometheus.NewCounterVec(
+	totalRequestCounter := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Namespace: "http",
-			Name:      "request_total",
-			Help:      "Number of get request",
+			Namespace: "kettle",
+			Name:      "request",
+			Help:      "Total no. of requests",
 		},
-		[]string{"path"},
+		[]string{"handler", "method", "code"},
+	)
+	concurrentRequestGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "kettle",
+		Name:      "concurrent",
+		Help:      "No. of concurrent requests"},
 	)
 
 	s := &PrometheusService{
-		responseTimeHistogram: responseTimeHistogram,
-		totalRequestCounter:   totalRequestCounter,
+		responseTimeHistogram:  responseTimeHistogram,
+		totalRequestCounter:    totalRequestCounter,
+		concurrentRequestGauge: concurrentRequestGauge,
 	}
 	err := prometheus.Register(s.responseTimeHistogram)
 	if err != nil && err.Error() != "duplicate metrics collector registration attempted" {
 		return nil, err
 	}
 	err = prometheus.Register(s.totalRequestCounter)
+	if err != nil && err.Error() != "duplicate metrics collector registration attempted" {
+		return nil, err
+	}
+	err = prometheus.Register(s.concurrentRequestGauge)
 	if err != nil && err.Error() != "duplicate metrics collector registration attempted" {
 		return nil, err
 	}
