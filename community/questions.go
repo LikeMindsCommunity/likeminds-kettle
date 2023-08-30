da5e8c5ce53246b8b9cc7d466c7950a00b509806
shubh-gupta00 <123367829+shubh-gupta00@users.noreply.github.com>
2023-08-30 15:33:57 +0530

    [LM-9573] Added support for option_only_for_self in Questions Api (#325)
    * Added support for option_only_for_self in Questions Api
    * added omitempty on questionAnswers

--- community/questions.go
+++ community/questions.go
@@ -22,6 +22,7 @@ type Question struct {
 	CanAddOptions       bool        `json:"can_add_options"`
 	IsCompulsory        bool        `json:"is_compulsory"`
 	IsAnswerEditable    bool        `json:"is_answer_editable"`
+	OptionsOnlyForSelf  *bool       `json:"options_only_for_self,omitempty"`
 	Tag                 string      `json:"tag,omitempty"`
 }
 
