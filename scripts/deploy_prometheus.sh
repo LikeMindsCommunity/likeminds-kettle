e1e0977e30c29872c800760cd5e2e9acaa9997a5
Natesh Relhan <nateshrelhan@Likemindss-MacBook-Pro.local>
2023-09-05 14:00:14 +0530

    prometheus beta deployment script added

---
+++ scripts/deploy_prometheus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /home/ubuntu/.profile
+cd /home/ubuntu/LikeMinds-Authentication/
+branch_name="$(git rev-parse --abbrev-ref HEAD)"
+if [ "$branch_name" = "master" ]
+then
+  curl -o ./.env https://prod-likeminds-media.s3.ap-south-1.amazonaws.com/environment/kettle-prod-dot-env-public
+  git pull origin master
+else
+  curl -o ./.env https://beta-likeminds-media.s3.ap-south-1.amazonaws.com/environment/Kettle-Beta-Dot-Env/.env
+  git pull origin "$branch_name"
+fi
+docker compose -f ./prometheus/docker-compose-prometheus.yml up -d --build
