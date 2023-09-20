b73c53a8a034639a6d46643ea3c0332b90b35131
Mahir-G2 <mahir.gupta@likeminds.community>
2023-09-20 23:54:46 +0530

    [LM-9583] Feed Poll changes after peer testing

--- poll/pollVote.go
+++ poll/pollVote.go
@@ -1,6 +1,7 @@
 package poll
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
@@ -88,8 +89,11 @@ func getPollVotesInternal(c *gin.Context, userId string, endPoint string) {
 	//If flow succeeds
 	dataResponse := apiCR.Response
 	if value, ok := dataResponse["votes"]; ok {
-		votesData := value.([]map[string]interface{})
+		votesData := []map[string]interface{}{}
 		userIds := []string{}
+
+		convertedVotesData, _ := json.Marshal(value)
+		_ = json.Unmarshal(convertedVotesData, &votesData)
 
 		//Fetch user ids
 		for _, voteData := range votesData {
