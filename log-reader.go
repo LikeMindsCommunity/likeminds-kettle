8089eeb669434b17551260593a9dcc1023de71b7
Natesh Relhan <nateshrelhan@Likemindss-MacBook-Pro.local>
2023-10-05 15:23:50 +0530

    log reader created and working

---
+++ log-reader.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type Log struct {
+	JsonPayload JsonPayload `json:"jsonPayload"`
+}
+
+type JsonPayload struct {
+	Msg string `json:"msg"`
+}
+
+type Msg struct {
+	Request Request `json:"request"`
+}
+
+type Request struct {
+	Headers Headers `json:"headers"`
+}
+
+type Headers struct {
+	Authorization []interface{} `json:"Authorization"`
+}
+
+func main() {
+	fileContent, err := os.Open("logs.json")
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println("The File is opened successfully...")
+
+	defer fileContent.Close()
+
+	byteResult, _ := ioutil.ReadAll(fileContent)
+
+	var logs []Log
+
+	json.Unmarshal(byteResult, &logs)
+
+	hashmap := make(map[string]int)
+	for i := 0; i < len(logs); i++ {
+		var msg Msg
+		json.Unmarshal([]byte(logs[i].JsonPayload.Msg), &msg)
+		AuthorizationValue := fmt.Sprint(msg.Request.Headers.Authorization)
+		hashmap[AuthorizationValue] = hashmap[AuthorizationValue] + 1
+	}
+	fmt.Print("Msg: ", len(hashmap))
+}
