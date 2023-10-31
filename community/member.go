be6ebeb5e15f7e119e88de2bec8d631f57420ee5
ankitgarg <ankit.garg@likeminds.community>
2023-10-31 13:13:45 +0530

    Changed the param name to included member roles.

--- community/member.go
+++ community/member.go
@@ -132,7 +132,7 @@ func getMemberInternal(c *gin.Context, userId string) {
 			ParamPage:                   page,
 			ParamMemberState:            c.Query(ParamMemberState),
 			ParamQuestionAnswersVersion: c.Query(ParamQuestionAnswersVersion),
-			ParamIncludedMemberStates:   c.Query(ParamIncludedMemberStates),
+			ParamIncludedMemberRoles:    c.Query(ParamIncludedMemberRoles),
 		}
 
 		//Send Request
