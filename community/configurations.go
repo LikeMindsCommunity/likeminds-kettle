5fc825a52ad771e5ae0ce599df311187a6963702
shubh-gupta00 <shubh.gupta@likeminds.community>
2024-01-03 19:29:39 +0530

    Added support of PATCH community configurations request

--- community/configurations.go
+++ community/configurations.go
@@ -6,6 +6,25 @@ import (
 	"github.com/nateshr/likeminds-authentication/utils"
 )
 
+// type communityConfigurationsValue struct {
+// }
+
+type UpdateCommunityConfigurationsRequest struct {
+	Type  string                 `json:"type" binding:"required"`
+	Value map[string]interface{} `json:"value" binding:"required"`
+}
+
+func parseCommunityConfigurationsRequest(c *gin.Context) (*UpdateCommunityConfigurationsRequest, error) {
+
+	var uccr UpdateCommunityConfigurationsRequest
+
+	if err := c.ShouldBindJSON(&uccr); err != nil {
+		return nil, err
+	}
+
+	return &uccr, nil
+}
+
 // Expose method to fetch community configurations for a community
 func GetCommunityConfigurations(c *gin.Context) {
 
@@ -20,6 +39,29 @@ func GetCommunityConfigurations(c *gin.Context) {
 	}
 
 	//Send Request to api/community/configurations
-	utils.SendRequest(c, utils.CoreService, FetchCommunityConfigurationsEndpoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
+	utils.SendRequest(c, utils.CoreService, CommunityConfigurationsEndpoint, utils.GETRequest,
+		utils.CreateHeaders(c, userId), params, nil)
+
+}
+
+// Expose method to update community configurations for a community
+func UpdateCommunityConfigurations(c *gin.Context) {
+
+	//Authorize User
+	userId := user.GetRequestingUserId(c)
+	if userId == "" {
+		return
+	}
+
+	//Parse request
+	uccr, err := parseCommunityConfigurationsRequest(c)
+	if err != nil {
+		utils.GeneralBadRequestError(c, err.Error())
+		return
+	}
+
+	//Send Request to api/community/configurations
+	utils.SendRequest(c, utils.CoreService, CommunityConfigurationsEndpoint, utils.PATCHRequest,
+		utils.CreateHeaders(c, userId), nil, uccr)
 
 }
