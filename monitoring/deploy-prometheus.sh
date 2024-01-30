0bd7a9e352df5a32a7efee0b783fd9ab0cd17675
Natesh Relhan <nateshrelhan@gmail.com>
2024-01-30 15:19:10 +0530

    prometheus env files changed (#397)
    Co-authored-by: Natesh Relhan <nateshrelhan@Likemindss-MacBook-Pro.local>

--- monitoring/deploy-prometheus.sh
+++ monitoring/deploy-prometheus.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 branch_name="$(git rev-parse --abbrev-ref HEAD)"
 if [ "$branch_name" = "master" ]; then
-  curl -o ./.env https://prod-likeminds-media.s3.ap-south-1.amazonaws.com/environment/kettle-prod-dot-env-public
+  curl -o ./.env https://likeminds-configs-prod.s3.ap-south-1.amazonaws.com/application-dot-envs-prod/kettle-prod/kettle-prod-dot-env-public
   git pull origin master
 else
-  curl -o ./.env https://beta-likeminds-media.s3.ap-south-1.amazonaws.com/environment/Kettle-Beta-Dot-Env/.env
+  curl -o ./.env https://likeminds-configs-beta.s3.ap-south-1.amazonaws.com/application-dot-envs-beta/kettle-beta/kettle-beta-dot-env-public
   git pull origin "$branch_name"
 fi
 set -a
