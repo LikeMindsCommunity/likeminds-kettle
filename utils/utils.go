59a17827c7934b47bd94bc5144fe41756e15252f
ankitgarg <ankit.garg@likeminds.community>
2024-03-13 11:58:08 +0530

    Restructuring of files.

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -10,44 +9,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/nateshr/likeminds-authentication/api_client"
 )
-
-// RequestParamsToMap Converts a struct to a map while maintaining the json alias as keys
-func RequestParamsToMap(obj interface{}) (newMap map[string]string, err error) {
-	data, err := json.Marshal(obj) // Convert to a json string
-	if err != nil {
-		return
-	}
-	err = json.Unmarshal(data, &newMap) // Convert to a map
-	return
-}
-
-// CreateHeaders Used to create headers for our internal APIs
-func CreateHeaders(c *gin.Context, userUniqueID string) map[string]interface{} {
-	headers := make(map[string]interface{})
-	if len(userUniqueID) > 0 {
-		headers[HeadersMemberId] = userUniqueID
-	}
-	headers[HeadersPlatformCode] = c.GetHeader(HeadersPlatformCode)
-	headers[HeadersVersionCode] = c.GetHeader(HeadersVersionCode)
-	headers[HeadersSdkSource] = c.GetHeader(HeadersSdkSource)
-	headers[HeadersDeviceId] = c.GetHeader(HeadersDeviceId)
-	headers[HeadersApiKey] = c.GetHeader(HeadersApiKey)
-	headers[HeadersAcceptVersion] = c.GetHeader(HeadersAcceptVersion)
-	headers[HeadersApiVersion] = c.GetHeader(HeadersApiVersion)
-	headers[HeaderMemberRole] = c.GetHeader(HeaderMemberRole)
-	return headers
-}
-
-// Add more headers to already existing headers
-func AddHeaders(c *gin.Context, headerValMap map[string]string) {
-
-	if headerValMap != nil {
-
-		for header, val := range headerValMap {
-			c.Request.Header.Add(header, val)
-		}
-	}
-}
 
 // Generate Response to be sent on request success
 func GenerateResponse(c *gin.Context, dataResponse map[string]interface{}, getProfileWidgets bool) {
