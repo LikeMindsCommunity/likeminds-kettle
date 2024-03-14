ed98ca37642639721725c12d193907ce8006f661
shubh-gupta00 <123367829+shubh-gupta00@users.noreply.github.com>
2024-03-14 17:56:44 +0530

    [LM-10861] User Topics Connection fixes (#425)

--- feed/like_post.go
+++ feed/like_post.go
@@ -113,10 +113,8 @@ func getPostLikesInternal(c *gin.Context, userId string, endPoint string) {
 		//Update user data in dataResponse
 		dataResponse["users"] = user_data
 
-		// if user Topics connection is enabled, fetch and update related data
-		if utils.UserTopicsConnectionEnabled(redisClient, headers) {
-			dataResponse = utils.FetchAndUpdateUserTopicsDataForResponse(redisClient, headers, dataResponse, user_ids)
-		}
+		// Update user topics data in dataResponse
+		dataResponse = utils.FetchAndUpdateUserTopicsDataForResponse(redisClient, headers, dataResponse, user_ids)
 	}
 
 	//Send response
