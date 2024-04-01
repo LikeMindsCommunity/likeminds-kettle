ddd3648720d432070298991a501649ebb8c920e3
animeshdas-lm <133637733+animeshdas-lm@users.noreply.github.com>
2024-04-01 17:49:33 +0530

    [LM-10596] Rate Limiting (#431)
    * feat(WIP): added ratelimiting middleware
    
    * feat(WIP): updated ratelimit middleware with key changes
    
    * feat: added env settings for rate limit and community tier data setting for community creation
    
    * feat: updated logic for create project and minor refactor
    
    * fix: repetitive API call for create project API
    
    * refactor: ratelimit logic and moved values to const
    
    ---------
    
    Co-authored-by: Animesh Das <animeshxdas@gmail.com>

--- sdk/project.go
+++ sdk/project.go
@@ -1,38 +1,43 @@
 package sdk
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"github.com/nateshr/likeminds-authentication/logging"
 	"github.com/nateshr/likeminds-authentication/user"
 	"github.com/nateshr/likeminds-authentication/utils"
 )
 
-//Platform | SDK platform schema
+// Platform | SDK platform schema
 type Platform struct {
 	Type        int    `json:"type"`
 	Package     string `json:"package"`
 	Certificate string `json:"certificate"`
 }
 
-//CommunityBasicBranding | community basic branding schema
+// CommunityBasicBranding | community basic branding schema
 type CommunityBasicBranding struct {
 	PrimaryColour string `json:"primary_colour"`
 }
 
-//CommunityAdvancedBranding | community advanced branding schema
+// CommunityAdvancedBranding | community advanced branding schema
 type CommunityAdvancedBranding struct {
 	HeaderColour       string `json:"header_colour"`
 	ButtonsIconsColour string `json:"buttons_icons_colour"`
 	TextLinksColour    string `json:"text_links_colour"`
 }
 
-//CommunityBrandingRequest | create SDK api key platform schema
+// CommunityBrandingRequest | create SDK api key platform schema
 type CommunityBrandingRequest struct {
 	Basic    CommunityBasicBranding    `json:"basic"`
 	Advanced CommunityAdvancedBranding `json:"advanced"`
 }
 
-//ProjectRequest | create SDK api key request schema
+// ProjectRequest | create SDK api key request schema
 type CreateProjectRequest struct {
 	CommunityName     string                   `json:"name" binding:"required"`
 	Branding          CommunityBrandingRequest `json:"branding"`
@@ -55,27 +60,32 @@ type UpdateProjectRequest struct {
 	IsJoinFormEnabled bool                     `json:"is_join_form_enabled"`
 }
 
-//CreateProject is used to create a new sdk project
+type SkulkBillingPlanResponse struct {
+	Success      bool   `json:"success"`
+	ErrorMessage string `json:"error_message"`
+}
+
+// CreateProject is used to create a new sdk project
 func CreateProject(c *gin.Context) {
 	Project(c, utils.POSTMethod)
 }
 
-//EditProject is used to edit an sdk project
+// EditProject is used to edit an sdk project
 func EditProject(c *gin.Context) {
 	Project(c, utils.PUTMethod)
 }
 
-//GetProject is used to get an existing sdk project
+// GetProject is used to get an existing sdk project
 func GetProject(c *gin.Context) {
 	Project(c, utils.GETMethod)
 }
 
-//DeleteProject is used to delete an existing sdk project
+// DeleteProject is used to delete an existing sdk project
 func DeleteProject(c *gin.Context) {
 	Project(c, utils.DELETEMethod)
 }
 
-//Project method handles community sdk project for each client
+// Project method handles community sdk project for each client
 func Project(c *gin.Context, method int) {
 
 	//Authorizing User
@@ -83,6 +93,7 @@ func Project(c *gin.Context, method int) {
 	if userId == "" {
 		return
 	}
+	headers := utils.CreateHeaders(c, userId)
 
 	switch method {
 	case utils.GETMethod:
@@ -93,7 +104,7 @@ func Project(c *gin.Context, method int) {
 		}
 
 		//Send Request
-		utils.SendRequest(c, utils.CoreService, ProjectEndpoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
+		utils.SendRequest(c, utils.CoreService, ProjectEndpoint, utils.GETRequest, headers, params, nil)
 
 	case utils.POSTMethod:
 
@@ -113,8 +124,49 @@ func Project(c *gin.Context, method int) {
 			return
 		}
 
-		//Send Request
-		utils.SendRequest(c, utils.CoreService, ProjectEndpoint, utils.POSTRequestRawBody, utils.CreateHeaders(c, botId), nil, projectRequest)
+		//Send Create Project request and recieve API key
+		respBytes, _ := utils.GetRequestResponse(c, utils.CoreService, ProjectEndpoint, utils.POSTRequestRawBody, utils.CreateHeaders(c, botId), nil, projectRequest)
+		if respBytes == nil {
+			return
+		}
+
+		//Parse response to get api key
+		projectResponse := make(map[string]interface{})
+		err = json.Unmarshal(respBytes, &projectResponse)
+		if err != nil {
+			utils.GeneralAPIError(c, err.Error())
+			return
+		}
+		// Fetch Community ID from API Key
+		redis := utils.GetRedisClientFromContext(c)
+		communityId, err := utils.FetchCommunityIdFromApiKey(redis, projectResponse["api_key"].(string))
+		if err != nil {
+			utils.GeneralAPIError(c, err.Error())
+			return
+		}
+
+		//Send Request to create billing plan for community
+		communityBillingRequest := map[string]interface{}{}
+		skulkRespBytes, statusCode, err := utils.GetRequestResponseWithoutContext(utils.SubscriptionService, fmt.Sprintf("/%s/%d", utils.BillingPlanEnpoint, communityId), utils.POSTRequestRawBody, headers, nil, communityBillingRequest)
+		if err != nil {
+			logging.Error(err.Error())
+			return
+		}
+		skulkResponse := SkulkBillingPlanResponse{}
+		err = json.Unmarshal(skulkRespBytes, &skulkResponse)
+		if err != nil {
+			logging.Error(err.Error())
+			return
+		}
+		if statusCode != http.StatusOK {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, utils.Response{
+				Success:      false,
+				ErrorMessage: skulkResponse.ErrorMessage,
+			})
+			return
+		}
+		//Send Response
+		utils.ParseResponse(c, respBytes, statusCode, true)
 
 	case utils.PUTMethod:
 
@@ -132,7 +184,7 @@ func Project(c *gin.Context, method int) {
 		}
 
 		//Send Request
-		utils.SendRequest(c, utils.CoreService, ProjectEndpoint, utils.PUTRequest, utils.CreateHeaders(c, userId), nil, projectRequest)
+		utils.SendRequest(c, utils.CoreService, ProjectEndpoint, utils.PUTRequest, headers, nil, projectRequest)
 
 	case utils.DELETEMethod:
 
@@ -142,7 +194,7 @@ func Project(c *gin.Context, method int) {
 		}
 
 		//Send Request
-		utils.SendRequest(c, utils.CoreService, ProjectEndpoint, utils.DELETERequest, utils.CreateHeaders(c, userId), nil, nil)
+		utils.SendRequest(c, utils.CoreService, ProjectEndpoint, utils.DELETERequest, headers, nil, nil)
 
 	}
 }
