1ca5464e6adb694b3a4731b006b5145758f549a8
Ketan Yadav <ketan@likeminds.community>
2024-05-28 10:58:00 +0530

    Update TLS header age param

--- middleware/const.go
+++ middleware/const.go
@@ -5,8 +5,8 @@ const RPM = 1 // RPM
 // StrictTransportSecurityHeaderKey | strict transport security (TLS) header key
 const StrictTransportSecurityHeaderKey = "Strict-Transport-Security"
 
-// StrictTransportSecurityHeaderValue | strict transport security (TLS) header value
-const StrictTransportSecurityHeaderValue = "max-age=31536000; includeSubDomains"
+// StrictTransportSecurityHeaderValue | strict transport security (TLS) header value, max-age (in hours)
+const StrictTransportSecurityHeaderValue = "max-age=720; includeSubDomains"
 
 // ContentTypeOptionsHeaderKey | api content type options header key
 const ContentTypeOptionsHeaderKey = "X-Content-Type-Options"
