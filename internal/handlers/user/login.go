014dda66e3898d4ccf31ec2261de121b0705c708
aditya-likeminds <aditya.singh@likeminds.community>
2024-09-04 12:57:50 +0530

    Updated user/login api for dashboard

--- internal/handlers/user/login.go
+++ internal/handlers/user/login.go
@@ -22,6 +22,23 @@ type LoginRequest struct {
 	User      User   `json:"user"`
 }
 
+func extractLoginDetailsFromVTM(vtm *constants.VerifyTokenMeta, lr *LoginRequest) *LoginRequest {
+
+	if vtm.EmailID != "" {
+		lr.User.Email = vtm.EmailID
+	}
+
+	if vtm.MobileNo != "" {
+		lr.User.MobileNo = vtm.MobileNo
+	}
+
+	if vtm.CountryCode != "" {
+		lr.User.CountryCode = vtm.CountryCode
+	}
+
+	return lr
+}
+
 // Login used when user is signing up and generate login and refresh tokens
 func Login(c *gin.Context) {
 
@@ -31,6 +48,11 @@ func Login(c *gin.Context) {
 		//If POST body params are missing
 		utils.GeneralBadRequestError(c, err.Error())
 		return
+	}
+
+	verifyTokenMeta, ok := c.Get(constants.ParamVTM)
+	if ok {
+		loginRequest = extractLoginDetailsFromVTM(verifyTokenMeta.(*constants.VerifyTokenMeta), loginRequest)
 	}
 
 	//Send Request
