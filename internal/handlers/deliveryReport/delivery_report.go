df158cff6c5c8ee5a7545ec28637e417f8ba11f2
Natesh Relhan <natesh@likeminds.community>
2024-10-16 13:55:51 +0530

    param fix

--- internal/handlers/deliveryReport/delivery_report.go
+++ internal/handlers/deliveryReport/delivery_report.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/nateshr/likeminds-authentication/internal/handlers/user"
 	"github.com/nateshr/likeminds-authentication/internal/utils"
+	"strings"
 )
 
 // GetDR is used to get delivery report of conversations
@@ -14,9 +15,13 @@ func GetDR(c *gin.Context) {
 		return
 	}
 
+	conversationIDs := c.QueryArray(ParamConversationIDs) // Use QueryArray for list of strings
+	// Join conversation IDs into a comma-separated string
+	conversationIDsStr := strings.Join(conversationIDs, ",")
+
 	params := map[string]string{
 		ParamChatroomID:      c.Query(ParamChatroomID),
-		ParamConversationIDs: c.Query(ParamConversationIDs),
+		ParamConversationIDs: conversationIDsStr,
 	}
 
 	//Send Request
