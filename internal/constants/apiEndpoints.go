7bb7537682432aaba10b9261cab8eb1640969694
shubh-gupta00 <shubh.gupta@likeminds.community>
2024-11-27 20:27:30 +0530

    fix: correct typo in dashboard route constant

--- internal/constants/apiEndpoints.go
+++ internal/constants/apiEndpoints.go
@@ -22,7 +22,7 @@ const (
 	UserOTPVerifyRoute            = "/user/otp/verify"
 	UserSocialLoginRoute          = "/user/social/login"
 	UserBlockRoute                = "/user/:user_uuid/block"
-	UserDahsboardkRoute           = "/user/dashboard"
+	UserDashboardRoute            = "/user/dashboard"
 )
 
 // Home Endpoints
