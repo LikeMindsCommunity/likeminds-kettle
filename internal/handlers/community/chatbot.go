60abdeb4732aae5e7b54d5d0cdd3d92ca1c621a8
shubh-gupta00 <shubh.gupta@likeminds.community>
2025-01-07 16:48:38 +0530

    fix: field name casing in CreateChatbotRequest for consistency

--- internal/handlers/community/chatbot.go
+++ internal/handlers/community/chatbot.go
@@ -22,7 +22,7 @@ type CreateChatbotMetaRequest struct {
 
 type CreateChatbotRequest struct {
 	UUID        string                   `json:"uuid"`
-	ImageUrl    string                   `json:"imageUrl"`
+	ImageUrl    string                   `json:"image_url"`
 	Name        string                   `json:"name" binding:"required"`
 	ChatbotMeta CreateChatbotMetaRequest `json:"chatbot_meta" binding:"required"`
 }
