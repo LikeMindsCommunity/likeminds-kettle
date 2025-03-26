ec6b94188028898516e935b19908055ad38236ad
shobhit-likeminds <shobhit.singh@likeminds.community>
2025-03-26 17:23:34 +0530

    fixed: cache invalidation for fetch_member_access key patterns
    change: implemented async or parallel task in /feed/post api through go routines

--- internal/handlers/user/fetch_access.go
+++ internal/handlers/user/fetch_access.go
@@ -20,49 +20,6 @@ type MemberAccessResponse struct {
 }
 
 // FetchMemberAccess | fetch member access for sent action
-// func FetchMemberAccess(c *gin.Context, accessType string, userId string) (bool, *MemberAccessResponse) {
-
-// 	//Params to be sent in the api/community_member/fetch_access request
-// 	params := map[string]string{
-// 		ParamAccessType: accessType,
-// 	}
-
-// 	//Params Validation
-// 	if params[ParamAccessType] == "" {
-// 		//If GET params are missing
-// 		utils.GETQueryParamsMissingError(c)
-// 		return false, nil
-// 	}
-
-// 	//Send Request
-// 	respBytes, statusCode := utils.GetRequestResponse(c, utils.CoreService, FetchUserAccessEndPoint, utils.GETRequest, utils.CreateHeaders(c, userId), params, nil)
-
-// 	//Validate response
-// 	apiCR := utils.ValidateClientResponse(c, respBytes, statusCode)
-// 	if apiCR == nil {
-// 		return false, nil
-// 	}
-
-// 	//If flow succeeds
-// 	dataResponse := apiCR.Response
-// 	response := MemberAccessResponse{
-// 		Access: false,
-// 		IsCm:   false,
-// 	}
-
-// 	//Create Data
-// 	if value, ok := dataResponse["access"]; ok {
-// 		response.Access = value.(bool)
-// 	}
-
-// 	if value, ok := dataResponse["is_cm"]; ok {
-// 		response.IsCm = value.(bool)
-// 	}
-
-// 	return true, &response
-// }
-
-// FetchMemberAccess | fetch member access for sent action
 func FetchMemberAccess(c *gin.Context, accessType string, userId string) (bool, *MemberAccessResponse) {
 
 	cachedResponse := getAccessDataAgainstUserIdAndAccessTypeFromCache(utils.GetRedisClientFromContext(c), accessType, userId)
@@ -183,7 +140,6 @@ func DeleteAccessDataAgainstUserIdAndAccessTypeFromCache(redisClient *redis.Clie
 		logging.Error(fmt.Sprintf("Error deleting access data from cache for key: %s, err: %v", cacheKey, err))
 		return err
 	}
-	logging.Info(fmt.Sprintf("Successfully deleted access data from cache for key: %s", cacheKey))
 	return nil
 }
 
@@ -200,7 +156,6 @@ func DeleteMultipleAccessDataAgainstUserIdAndAccessTypeFromCache(redisClient *re
 		logging.Error(fmt.Sprintf("Error deleting access data from cache for multiple keys: %s, err: %v", keyPatterns, err))
 		return err
 	}
-	logging.Info(fmt.Sprintf("Successfully deleted access data from cache for MULTIPLE keys: %s", keyPatterns))
 	return nil
 }
 
@@ -214,6 +169,5 @@ func DeleteAllAccessDataAgainstUserIdAndAccessTypeFromCache(redisClient *redis.C
 		logging.Error(fmt.Sprintf("Error deleting access data from cache for key: %s, err: %v", cacheKey, err))
 		return err
 	}
-	logging.Info(fmt.Sprintf("Successfully deleted access data from cache for ALL keys: %s", cacheKey))
 	return nil
 }
